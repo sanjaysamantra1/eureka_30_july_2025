b0e213db85d75a1f8c80ecbeebd68fce348de932
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-08-10 22:03:12 +0530

    day 11

---
+++ javascript/day_11_aug_10/math.mjs
@@ -0,0 +1,11 @@
+// export default let a = 10;
+
+export default function add(a, b) {
+    console.log(`Addition of ${a} and ${b} is ${a + b}`);
+}
+export function sub(a, b) {
+    console.log(`subtraction of ${a} and ${b} is ${a - b}`);
+}
+export function mul(a, b) {
+    console.log(`multiplication of ${a} and ${b} is ${a * b}`);
+}
---
+++ javascript/day_11_aug_10/math_test.mjs
@@ -0,0 +1,5 @@
+import addition, { mul, sub } from "./math.mjs";
+
+addition(20,10);
+sub(20,10);
+mul(20,10);
